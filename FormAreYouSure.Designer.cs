ddc21560da0adb9f9c3d7a214c1bd06b65a06d05
Wyatt Bussell <bussellw@mr1.local>
2024-04-23 17:26:55 -0500

    created new form for checking inf the user is sure to delete an item (part or product)

---
+++ FormAreYouSure.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WyattBussellC968Software1C_
+{
+    partial class FormAreYouSure
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonContinue = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.labelAreYouSure = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonContinue
+            // 
+            this.buttonContinue.Location = new System.Drawing.Point(33, 56);
+            this.buttonContinue.Name = "buttonContinue";
+            this.buttonContinue.Size = new System.Drawing.Size(75, 23);
+            this.buttonContinue.TabIndex = 0;
+            this.buttonContinue.Text = "Continue";
+            this.buttonContinue.UseVisualStyleBackColor = true;
+            this.buttonContinue.Click += new System.EventHandler(this.buttonContinue_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(114, 56);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 1;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // labelAreYouSure
+            // 
+            this.labelAreYouSure.AutoSize = true;
+            this.labelAreYouSure.Location = new System.Drawing.Point(71, 25);
+            this.labelAreYouSure.Name = "labelAreYouSure";
+            this.labelAreYouSure.Size = new System.Drawing.Size(72, 13);
+            this.labelAreYouSure.TabIndex = 2;
+            this.labelAreYouSure.Text = "Are you sure?";
+            // 
+            // FormAreYouSure
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(230, 121);
+            this.ControlBox = false;
+            this.Controls.Add(this.labelAreYouSure);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonContinue);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormAreYouSure";
+            this.Text = "Are You Sure?";
+            this.Load += new System.EventHandler(this.FormAreYouSure_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonContinue;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Label labelAreYouSure;
+    }
+}
---
+++ FormAreYouSure.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WyattBussellC968Software1C_
+{
+    public partial class FormAreYouSure : Form
+    {
+        public FormAreYouSure()
+        {
+            InitializeComponent();
+        }
+
+        private void FormAreYouSure_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonContinue_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
